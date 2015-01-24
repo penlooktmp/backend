a43ab0ff1178a7288e5fc4e58abaac17f2e32988
naviet <naviet2308@gmail.com>
2015-01-24 20:21:02 +0700

    create unit test TableTest.php

---
+++ app/TableTest.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Copyright (C) 2014 Penlook
+ *                    Vietnam
+ *                    http://www.penlook.com
+ *                    support@penlook.com
+ *
+ * Authors:
+ *  Viet Nguyen <vietna@penlook.com>
+ *
+ */
+
+require_once __DIR__ . '/../TestCase.php';
+
+use App\Table;
+
+/**
+ * Table Test
+ *
+ * @category   Penlook Application
+ * @package    App
+ * @author     Viet Nguyen <vietna@penlook.com>
+ * @copyright  Penlook Development Team
+ * @license    Commercial
+ * @version    1.0
+ * @link       http://penlook.com
+ * @since      Class available since Release 1.0
+ */
+class TableTest extends TestCase
+{
+    private $table;
+
+    public function __construct() {
+        // $this->table = new Table();
+    }
+
+    public function testInitialize(){
+        // unknown
+    }
+}
