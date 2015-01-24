ff60b161915607b904b6f3719e422f7c8bd2fdf6
naviet <naviet2308@gmail.com>
2015-01-24 19:10:37 +0700

    create unit test ViewTest.php TableTest.php

--- app/ViewTest.php
+++ app/ViewTest.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/../TestCase.php';
 use App\View;
 
 /**
- * App Test
+ * View Test
  *
  * @category   Penlook Application
  * @package    App
@@ -44,19 +44,59 @@ class ViewTest extends TestCase
         $this->assertEquals($output, $expect);
     }
 
-    public function testStart(){
+    public function testSite(){
+        $_SERVER['SERVER_NAME'] = 'penlook.com';
+
+        $input = 'abc';
+        $expect = 'http://penlook.com/abc';
+        $output = $this->view->site($input);
+        $this->assertEquals($output, $expect);
+    }
+
+    public function testJson(){
+        $input = new \stdClass();
+        $input->test1 = "test1";
+        $input->test2 = "test2";
+
+        $expect =  '<script>var json = {"test1":"test1","test2":"test2"};</script>';
+        $output = $this->view->json($input);
+        $this->assertNotNull($output);
+        $this->assertEquals($output, $expect);
+    }
+
+    public function testImg()
+    {
+        $input = "testImg.png";
+        $expect = 'http:///img/testImg.png';
+        $output = $this->view->img($input);
+        $this->assertEquals($output, $expect);
+    }
+
+    public function testCss(){
+        $input = "test";
+
+        $expect = '<link rel="stylesheet" type="text/css" href="test.css" />';
+
+        $output = $this->view->css($input);
+        $this->assertEquals($output, $expect);
+    }
+
+    public function testJs()
+    {
+        $input = "test";
+        $expect = "<script>(function(){function e(){var e=document.createElement('script');e.type='text/javascript';e.async=true;e.src='test.js';var t=document.getElementsByTagName('script')[0];t.parentNode.insertBefore(e,t)}if(window.attachEvent){window.attachEvent('onload',e);}else {window.addEventListener('load',e,false)}})()</script>";
+
+        $output = $this->view->js($input);
+        $this->assertEquals($output, $expect);
+    }
+
+    public function testLess()
+    {
         // unknown
     }
 
-    public function testSetMode(){
-        // unknown
-    }
-
-    public function testGet(){
-        // unknown
-    }
-
-    public function testSet(){
+    public function testcoffee()
+    {
         // unknown
     }
 }
